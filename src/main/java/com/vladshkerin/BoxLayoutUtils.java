19fc74997dfef730809490ef4b4aec1fcfe0327d
Vladimir Shkerin <vladshkerin@gmail.com>
2016-01-24 13:06:04 +0500

    Refactoring class BoxLayoutUtils

--- src/main/java/com/vladshkerin/BoxLayoutUtils.java
+++ src/main/java/com/vladshkerin/BoxLayoutUtils.java
@@ -3,21 +3,14 @@ package com.vladshkerin;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Класс для удобной работы с менеджером BoxLayout
+ * <p/>
+ * Код взят из книги:
+ * Название: Swing эффектные пользовательские интерфейсы
+ * Автор: Иван Портянкин
+ */
 public class BoxLayoutUtils {
-
-    // задаёт единое выравнивание по оси X для группы компонентов
-    public static void setGroupAlignmentX(float alignment, JComponent... cs) {
-        for (JComponent c : cs) {
-            c.setAlignmentX(alignment);
-        }
-    }
-
-    // задаёт единое выравнивание по оси Y для группы компонентов
-    public static void setGroupAlignmentY(float alignment, JComponent... cs) {
-        for (JComponent c : cs) {
-            c.setAlignmentY(alignment);
-        }
-    }
 
     // возвращает панель с установленным вертикальным блочным расположением
     public static JPanel createVerticalPanel() {
@@ -33,13 +26,27 @@ public class BoxLayoutUtils {
         return p;
     }
 
+    // создаёт вертикальную распорку фиксированного размера
+    public static Component createVerticalStrut(int size) {
+        return Box.createRigidArea(new Dimension(0, size));
+    }
+
     // создаёт горизонтальную распорку фиксированного размера
     public static Component createHorizontalStrut(int size) {
         return Box.createRigidArea(new Dimension(size, 0));
     }
 
-    // создаёт вертикальную распорку фиксированного размера
-    public static Component createVerticalStrut(int size) {
-        return Box.createRigidArea(new Dimension(0, size));
+    // задаёт единое выравнивание по оси X для группы компонентов
+    public static void setGroupAlignmentX(float alignment, JComponent... cs) {
+        for (JComponent c : cs) {
+            c.setAlignmentX(alignment);
+        }
+    }
+
+    // задаёт единое выравнивание по оси Y для группы компонентов
+    public static void setGroupAlignmentY(float alignment, JComponent... cs) {
+        for (JComponent c : cs) {
+            c.setAlignmentY(alignment);
+        }
     }
 }
