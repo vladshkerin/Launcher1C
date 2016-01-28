434df638262af0f96b2d3e6799497f68749cd0d6
Vladimir Shkerin <vladshkerin@gmail.com>
2016-01-28 11:23:41 +0500

    Global refactoring class Command

--- src/main/java/com/vladshkerin/Command.java
+++ src/main/java/com/vladshkerin/Command.java
@@ -1,83 +1,24 @@
 package com.vladshkerin;
 
-import com.vladshkerin.exception.NotFoundPathException;
+import com.vladshkerin.exception.NotFoundSettingException;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Class for creation, checking and receiving commands 1C
  */
 public class Command {
 
-    private static String path1c;
-    private static String pathBackup;
-    private static String pathBase;
-    private static String file1c;
-    private static String fileTest;
-    private static String backBase;
+    private static Logger log = Logger.getLogger(Settings.class.getName());
     private static String lang;
 
     static {
-        path1c = "C:\\Program Files\\1cv82\\8.2.19.90\\bin\\";
-        pathBase = "C:\\base1c";
-        pathBackup = "C:\\backup";
-        file1c = "1cv8.exe";
-        fileTest = "chdbfl.exe";
-        backBase = "base1c_" +
-                new SimpleDateFormat("MM_yyyy").format(System.currentTimeMillis());
         lang = Resource.getCurrentLocale().getLanguage();
     }
 
-    /**
-     * Checks all the path to start and maintenance 1C.
-     *
-     * @throws NotFoundPathException
-     */
-    public static void checkDefaultPath() throws NotFoundPathException {
-        if (!(new File(path1c).exists())) {
-            String[] mas = new String[]{
-                    "C:\\Program Files\\1cv82\\8.2.19.130\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.19.130\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.19.90\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.19.90\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.19.83\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.19.83\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.19.76\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.19.76\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.18.109\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.18.109\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.17.169\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.17.169\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.16.362\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.16.362\\bin\\",
-                    "C:\\Program Files\\1cv82\\8.2.15.294\\bin\\",
-                    "C:\\Program Files (x86)\\1cv82\\8.2.15.294\\bin\\"
-            };
-
-            boolean flag = false;
-            File file1c;
-            for (String str : mas) {
-                file1c = new File(str);
-                if (file1c.exists()) {
-                    path1c = str;
-                    flag = true;
-                    break;
-                }
-            }
-
-            if (!flag) {
-                throw new NotFoundPathException(path1c);
-            }
-        }
-
-        if (!(new File(pathBase).exists())) {
-            throw new NotFoundPathException(pathBase);
-        }
-
-        if (!(new File(pathBackup).exists())) {
-            throw new NotFoundPathException(pathBackup);
-        }
+    private Command() {
+        //TODO empty
     }
 
     /**
@@ -85,63 +26,83 @@ public class Command {
      */
     public static String[] getString(Operations operation) {
         String[] cmd = new String[]{};
-        switch (operation) {
-            case ENTERPRISE:
-                cmd = new String[]{
-                        path1c + file1c, "ENTERPRISE", "/F" + pathBase, "/N" + "Ревизор",
-                        "/L" + lang, "/DisableStartupMessages"
-                };
-                break;
-            case CONFIG:
-                cmd = new String[]{
-                        path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server",
-                        "/L" + lang, "/DisableStartupMessages"
-                };
-                break;
-            case CHECK:
-                cmd = new String[]{
-                        path1c + fileTest
-                };
-                break;
-            case UNLOAD_DB:
-                cmd = new String[]{
-                        path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server", "/P" + "server",
-                        "/L" + lang, "/DumpIB" + pathBackup + "\\" + backBase,
-                        "/DisableStartupMessages"
-                };
-                break;
-            case KILL:
-                cmd = new String[]{
-                        "taskkill", "/F", "/IM", "1cv8.exe"
-                };
-                break;
-            case COPY:
-                cmd = new String[]{
-                        "copy", "/Y",
-                        pathBackup + "\\" + backBase + ".bak",
-                        pathBackup + "\\" + backBase
-                };
-                break;
-            case UPDATE:
-                cmd = new String[]{
-                        path1c + file1c, "ENTERPRISE", "/F" + pathBase, "/N" + "server", "/P" + "server",
-                        "/L" + lang, "/C" + "OBMEN#ЦентральныйОбменРевизии#*",
-                        "/DisableStartupMessages"
-                };
-                break;
-            case UPGRADE:
-                cmd = new String[]{
-                        path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server", "/P" + "server",
-                        "/L" + lang, "/UpdateDBCfg",
-                        "/DisableStartupMessages"
-                };
-                break;
-            case TEST:
-                cmd = new String[]{
-                        path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server", "/P" + "server",
-                        "/L" + lang, "/IBCheckAndRepair -ReIndex -LogIntegrity",
-                        "/DisableStartupMessages"
-                };
+        try {
+            String path1c, pathBase, pathBackup, file1c, fileTest, fileBackup;
+            switch (operation) {
+                case ENTERPRISE:
+                    path1c = Settings.getString("path.1c");
+                    pathBase = Settings.getString("path.base");
+                    file1c = Settings.getString("file.1c");
+                    cmd = new String[]{
+                            path1c + file1c, "ENTERPRISE", "/F" + pathBase, "/N" + "Ревизор",
+                            "/L" + lang, "/DisableStartupMessages"
+                    };
+                    break;
+                case CONFIG:
+                    path1c = Settings.getString("path.1c");
+                    pathBase = Settings.getString("path.base");
+                    file1c = Settings.getString("file.1c");
+                    cmd = new String[]{
+                            path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server",
+                            "/L" + lang, "/DisableStartupMessages"
+                    };
+                    break;
+                case CHECK:
+                    path1c = Settings.getString("path.1c");
+                    fileTest = Settings.getString("file.test");
+                    cmd = new String[]{
+                            path1c + fileTest
+                    };
+                    break;
+                case UNLOAD_DB:
+                    path1c = Settings.getString("path.1c");
+                    pathBase = Settings.getString("path.base");
+                    pathBackup = Settings.getString("path.backup");
+                    file1c = Settings.getString("file.1c");
+                    fileBackup = Settings.getString("file.backup");
+                    cmd = new String[]{
+                            path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server", "/P" + "server",
+                            "/L" + lang, "/DumpIB" + pathBackup + "\\" + fileBackup,
+                            "/DisableStartupMessages"
+                    };
+                    break;
+                case KILL:
+                    cmd = new String[]{
+                            "taskkill", "/F", "/IM", "1cv8.exe"
+                    };
+                    break;
+                case UPDATE:
+                    path1c = Settings.getString("path.1c");
+                    pathBase = Settings.getString("path.base");
+                    file1c = Settings.getString("file.1c");
+                    cmd = new String[]{
+                            path1c + file1c, "ENTERPRISE", "/F" + pathBase, "/N" + "server", "/P" + "server",
+                            "/L" + lang, "/C" + "OBMEN#ЦентральныйОбменРевизии#*",
+                            "/DisableStartupMessages"
+                    };
+                    break;
+                case UPGRADE:
+                    path1c = Settings.getString("path.1c");
+                    pathBase = Settings.getString("path.base");
+                    file1c = Settings.getString("file.1c");
+                    cmd = new String[]{
+                            path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server", "/P" + "server",
+                            "/L" + lang, "/UpdateDBCfg",
+                            "/DisableStartupMessages"
+                    };
+                    break;
+                case TEST:
+                    path1c = Settings.getString("path.1c");
+                    pathBase = Settings.getString("path.base");
+                    file1c = Settings.getString("file.1c");
+                    cmd = new String[]{
+                            path1c + file1c, "CONFIG", "/F" + pathBase, "/N" + "server", "/P" + "server",
+                            "/L" + lang, "/IBCheckAndRepair -ReIndex -LogIntegrity",
+                            "/DisableStartupMessages"
+                    };
+            }
+        } catch (NotFoundSettingException e) {
+            log.log(Level.CONFIG, e.getMessage());
         }
         return cmd;
     }
