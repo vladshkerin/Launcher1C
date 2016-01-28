953b48e3889e0fb99445ab54d23161b90b8ca659
Vladimir Shkerin <vladshkerin@gmail.com>
2016-01-28 10:55:03 +0500

    Add all operation "All"

--- src/main/java/com/vladshkerin/Operations.java
+++ src/main/java/com/vladshkerin/Operations.java
@@ -4,12 +4,12 @@ package com.vladshkerin;
  * Operations run the program 1C.
  */
 public enum Operations {
+    ALL,
     ENTERPRISE,
     CONFIG,
     CHECK,
     UNLOAD_DB,
     KILL,
-    COPY,
     UPGRADE,
     UPDATE,
     TEST
