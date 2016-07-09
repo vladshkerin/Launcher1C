d93d5d9d6588ebca9859e31404038176334b982a
Vladimir Shkerin <vladshkerin@gmail.com>
2016-07-09 11:52:48 +0500

    Translated comments into English

--- src/main/java/com/vladshkerin/BoxLayoutUtils.java
+++ src/main/java/com/vladshkerin/BoxLayoutUtils.java
@@ -1,59 +1,60 @@
-package com.vladshkerin;
+package ru.vladimirshkerin.models;
 
 import javax.swing.*;
 import java.awt.*;
 
 /**
- * Класс для удобной работы с менеджером BoxLayout
+ * Class for easy work with a BoxLayout Manager
  * <p/>
- * Код взят из книги:
- * Название: Swing эффектные пользовательские интерфейсы
- * Автор: Иван Портянкин
+ * Code taken from the book:
+ * Title: Swing эффектные пользовательские интерфейсы (Издание 2-е)
+ * Author: Иван Портянкин
+ * Publisher: Лори
  */
 public class BoxLayoutUtils {
 
-    // возвращает панель с установленным вертикальным блочным расположением
+    // Returns the panel with the installed vertical block layout.
     public static JPanel createVerticalPanel() {
         JPanel p = new JPanel();
         p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
         return p;
     }
 
-    // возвращает панель с установленным горизонтальным блочным расположением
+    // Returns the panel with the installed horizontal block layout.
     public static JPanel createHorizontalPanel() {
         JPanel p = new JPanel();
         p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
         return p;
     }
 
-    // создаёт вертикальную распорку фиксированного размера
+    // Creates a vertical strut fixed size.
     public static Component createVerticalStrut(int size) {
         return Box.createRigidArea(new Dimension(0, size));
     }
 
-    // создаёт горизонтальную распорку фиксированного размера
+    // Creates a horizontal strut of a fixed size.
     public static Component createHorizontalStrut(int size) {
         return Box.createRigidArea(new Dimension(size, 0));
     }
 
-    // создаёт вертикальную пружыну
+    // Creates a vertical spring.
     public static Component createVerticalGlue() {
         return Box.createVerticalGlue();
     }
 
-    // создаёт горизонтальную пружыну
+    // Creates a horizontal spring.
     public static Component createHorizontalGlue() {
         return Box.createHorizontalGlue();
     }
 
-    // задаёт единое выравнивание по оси X для группы компонентов
+    // Sets a common alignment along the X axis for component groups.
     public static void setGroupAlignmentX(float alignment, JComponent... cs) {
         for (JComponent c : cs) {
             c.setAlignmentX(alignment);
         }
     }
 
-    // задаёт единое выравнивание по оси Y для группы компонентов
+    // Sets a common alignment axis Y for component group.
     public static void setGroupAlignmentY(float alignment, JComponent... cs) {
         for (JComponent c : cs) {
             c.setAlignmentY(alignment);
